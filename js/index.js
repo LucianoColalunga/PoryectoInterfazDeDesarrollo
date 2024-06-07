1cbe213e2c0c8739edab50f0ee897f7384442b66
Luciano <129859436+LucianoColalunga@users.noreply.github.com>
2024-06-07 20:02:56 -0300

    Pasando proyecto de carpeta y subirlo a GH
    Cambie el proyecto de carpeta donde se ingreso el resto del proyecto para subir todo al repositorio

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('div.container')
+
+retornarCardHtml =  (producto) => {
+    return `<div class="card">
+                <div class="card-image">${producto.imagen}</div>
+                <div class="card-name">${producto.nombre}</div>
+                <div class="card-price">${producto.precio}</div>
+                <div class="card-button">
+                    <button class="button button-outline button-add" id="" title="Clic para agregar al carrito">+</button>
+                </div>
+            </div>`
+}
+const cargarProductos = (array) => {
+    if(array.length > 0){
+        array.forEach(producto => {
+            container.innerHTML += retornarCardHtml(producto)
+        });
+    }
+}
+cargarProductos(productos)
