adaf15cc20810d510c48a1af943d2f00472a94d7
luciano <129859436+LucianoColalunga@users.noreply.github.com>
2024-06-26 18:34:39 -0300

    Buttons were added where objects add up to the cart
    Functionalities were added where the loved product clicks and adds to the cart

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,12 @@
-const  carritoFrutas = [];
+const carrito = [];
 
+function agregarACarrito(id) {
+    const producto = productos.find(p => p.id == id);
+    if (producto) {
+        carrito.push(producto);
+        console.log(carrito); 
+    }
+}
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
-const container = document.querySelector('div.container')
+const container = document.querySelector('div.container');
 
-retornarCardHtml =  (producto) => {
+const retornarCardHtml = (producto) => {
     return `<div class="card">
                 <div class="card-image">${producto.imagen}</div>
                 <div class="card-name">${producto.nombre}</div>
                 <div class="card-price">${producto.precio}</div>
                 <div class="card-button">
-                    <button class="button button-outline button-add" id="" title="Clic para agregar al carrito">+</button>
+                    <button class="button button-outline button-add" id="${producto.id}" title="Clic para agregar al carrito">+</button>
                 </div>
-            </div>`
-}
-const cargarProductos = (array) => {
-    if(array.length > 0){
-        array.forEach(producto => {
-            container.innerHTML += retornarCardHtml(producto)
-        });
+            </div>`;
+};
+
+function activarClickEnCarrito() {
+    const botonesCarrito = document.querySelectorAll('button.button.button-outline.button-add');
+    if (botonesCarrito !== null) {
+        for (let boton of botonesCarrito) {
+            boton.addEventListener("click", (e) => {
+                agregarACarrito(e.target.id);
+            });
+        }
     }
 }
-cargarProductos(productos)
+
+const cargarProductos = (array) => {
+    if (array.length > 0) {
+        array.forEach(producto => {
+            container.innerHTML += retornarCardHtml(producto);
+        });
+    }
+    activarClickEnCarrito();
+};
+
+cargarProductos(productos);
